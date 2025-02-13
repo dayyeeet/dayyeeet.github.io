8a8b7d435d3f4472bc14274af1e1daa4e9e6fad2
David <david@cappell.net>
2025-02-13 01:27:05 +0100

    refactor: spelling

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,10 +7,10 @@ export default function Hero() {
             className={"w-full h-full bg-linear-to-b from-transparent via-transparent to-background absolute top-0 left-0"}/>
         <div className={"w-full z-20 space-y-6 px-10 sm:px-20"}>
             <div className={"text-foreground text-5xl xl:text-7xl tracking-tight font-bold"}>
-                Hi, Im David Cappell
+                Hi, I&apos;m David Cappell
             </div>
             <div className={"text-foreground/70 text-2xl xl:text-3xl tracking-tight max-w-3/4 sm:max-w-2/3 md:max-w-1/2"}>
-                A Computer Science Student specializing in full-stack development with React and strong focus on backend
+                A computer science student specializing in full-stack development with React and strong focus on backend
                 technologies.
             </div>
         </div>
